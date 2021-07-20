78a6669106fac82d950066c3dc91c6bd445e8019
Bilkis ismail <bilkisismail07@gmail.com>
2021-07-21 03:51:40 +0700

    base template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="row mt-5 justify-content-md-center">
+        <div className="col-lg-6">
+          <div className="card">
+            <form className="row g-3 card-body">
+              <div className="col-md-6">
+                <label htmlFor="inputEmail4" className="form-label">
+                  Email
+                </label>
+                <input type="email" className="form-control" id="inputEmail4" />
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="inputPassword4" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inputPassword4"
+                />
+              </div>
+              <div className="col-12">
+                <label htmlFor="inputAddress" className="form-label">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputAddress"
+                  placeholder="1234 Main St"
+                />
+              </div>
+              <div className="col-12">
+                <label htmlFor="inputAddress2" className="form-label">
+                  Address 2
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputAddress2"
+                  placeholder="Apartment, studio, or floor"
+                />
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="inputCity" className="form-label">
+                  City
+                </label>
+                <input type="text" className="form-control" id="inputCity" />
+              </div>
+              <div className="col-md-4">
+                <label htmlFor="inputState" className="form-label">
+                  State
+                </label>
+                <select id="inputState" className="form-select" defaultValue="">
+                  <option value="" disabled>
+                    Choose...
+                  </option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div className="col-md-2">
+                <label htmlFor="inputZip" className="form-label">
+                  Zip
+                </label>
+                <input type="text" className="form-control" id="inputZip" />
+              </div>
+              <div className="col-12">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="gridCheck"
+                  />
+                  <label className="form-check-label" htmlFor="gridCheck">
+                    Check me out
+                  </label>
+                </div>
+              </div>
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary">
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
